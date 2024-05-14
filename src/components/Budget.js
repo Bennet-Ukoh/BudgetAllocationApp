36576488ee746fb007907d0574ff4fa8cded201e
Bennet-Ukoh <bennetukoh@gmail.com>
2024-05-14 15:39:19 +0100

    budget made editable

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,19 +1,23 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
-  const { budget } = useContext(AppContext);
-  const [newBudget, setNewBudget] = useState(budget);
+  const { budget, dispatch } = useContext(AppContext);
+
   const handleBudgetChange = (event) => {
-    setNewBudget(event.target.value);
+    dispatch({
+      type: "SET_BUDGET",
+      payload: event.target.value,
+    });
   };
+
   return (
     <div className="alert alert-secondary">
-      <span>Budget: £{budget}</span>
+      <span>Budget: £ </span>
       <input
         type="number"
         step="10"
-        value={newBudget}
+        value={budget}
         onChange={handleBudgetChange}
       ></input>
     </div>
